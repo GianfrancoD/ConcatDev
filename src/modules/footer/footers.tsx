4155630c75ae1af21e85cd354936270ed4ac1ed5
GianfrancoD <gianfranco.dfiore@gmail.com>
2025-03-14 12:21:05 -0400

    Web ConcatDev

--- src/modules/footer/footers.tsx
+++ src/modules/footer/footers.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import logotipo from "../../assets/concatdev2.png";
+import { useLanguageContext } from "../../provider";
 
 export const Footer = () => {
+  const { t } = useLanguageContext();
   return (
     <footer
       className="py-12 border-t border-white/10 bg-gradient-to-t from-[#312760] to-[#1a1438] "
@@ -16,78 +18,90 @@ export const Footer = () => {
               className="min-[425px]:h-[30vh] min-[425px]:w-auto min-[425px]:px-auto min-[425px]:-mt-10  max-[425px]:px-10"
             />
             <p className="text-gray-400">
-              Creando experiencias digitales excepcionales que generan
-              resultados.
+              {t(
+                "footer.description",
+                "Creando experiencias digitales excepcionales que generan resultados."
+              )}
             </p>
             <a
               href="/"
               className="text-gray-500 bg-gradient-to-r from-[#fcae60] to-[#ff8fb1] bg-clip-text text-transparent"
             >
-              <span> Política de privacidad</span>
+              <span>
+                {" "}
+                {t("footer.privacyPolicy", "Política de privacidad")}
+              </span>
             </a>
-            {/* <a
+            <a
               href="/"
               className="text-gray-500 bg-gradient-to-r from-[#fcae60] to-[#ff8fb1] bg-clip-text text-transparent"
             >
-              <span> Política de Cookies</span>
-            </a> */}
+              <br />
+              <span> {t("footer.cookiesPolicy", "Política de Cookies")}</span>
+            </a>
           </div>
           <div>
-            <h4 className="text-white font-semibold mb-4">Servicios</h4>
+            <h4 className="text-white font-semibold mb-4">
+              {t("footer.services.title", "Servicios")}
+            </h4>
             <ul className="space-y-2 text-orange-300">
               <li>
                 <a href="/" className="hover:text-white transition-colors">
-                  Desarrollo
+                  {t("footer.services.development", "Desarrollo")}
                 </a>
               </li>
               <li>
                 <a href="/" className="hover:text-white transition-colors">
-                  Marketing
+                  {t("footer.services.marketing", "Marketing")}
                 </a>
               </li>
               <li>
                 <a href="/" className="hover:text-white transition-colors">
-                  Optimización
+                  {t("footer.services.optimization", "Optimización")}
                 </a>
               </li>
             </ul>
           </div>
           <div>
-            <h4 className="text-white font-semibold mb-4">Navegación</h4>
+            <h4 className="text-white font-semibold mb-4">
+              {t("footer.navigation.title", "Navegación")}
+            </h4>
             <ul className="space-y-2 text-orange-300">
               <li>
                 <a href="/" className="hover:text-white transition-colors">
-                  About
+                  {t("footer.navigation.about", "About")}
                 </a>
               </li>
               <li>
                 <a href="/" className="hover:text-white transition-colors">
-                  Proyectos
+                  {t("footer.navigation.projects", "Proyectos")}
                 </a>
               </li>
               <li>
                 <a href="/" className="hover:text-white transition-colors">
-                  Process
+                  {t("footer.navigation.process", "Process")}
                 </a>
               </li>
               <li>
                 <a href="/" className="hover:text-white transition-colors">
-                  Contact
+                  {t("footeeer.navigation.contact", "Contact")}
                 </a>
               </li>
             </ul>
           </div>
           <div>
-            <h4 className="text-white font-semibold mb-4">Redes</h4>
+            <h4 className="text-white font-semibold mb-4">
+              {t("footer.social.title", "Redes")}
+            </h4>
             <ul className="space-y-2 text-orange-300">
               <li>
                 <a href="/" className="hover:text-white transition-colors">
-                  Instagram
+                  {t("footer.social.instagram", "Instagram")}
                 </a>
               </li>
               <li>
                 <a href="/" className="hover:text-white transition-colors">
-                  GitHub
+                  {t("footer.social.github", "GitHub")}
                 </a>
               </li>
             </ul>
@@ -95,8 +109,8 @@ export const Footer = () => {
         </div>
         <div className="mt-12 pt-8 border-t border-white/10 text-center text-gray-400">
           <p>
-            &copy; {new Date().getFullYear()} ConcatDev. Todos los derechos
-            reservados por Gianfranco Di Berardino.
+            &copy; {new Date().getFullYear()} ConcatDev. All rights reserved by
+            Gianfranco Di Berardino.
           </p>
         </div>
       </div>
